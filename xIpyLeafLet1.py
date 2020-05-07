0dc8c336a5940710eeadbf88ea63bd6b4dd76637
Thierry Dumont <tdumont@math.univ-lyon1.fr>
2020-05-07 11:24:45 +0200

    minor changes

--- xIpyLeafLet1.py
+++ xIpyLeafLet1.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 from ipyleaflet import Map, Marker,Polyline, FullScreenControl, WidgetControl, MarkerCluster, CircleMarker, basemaps, AntPath
 from ipywidgets import IntSlider,jslink
 import gpxpy
@@ -36,10 +33,6 @@ gpx_file= open(filegpx,"r")
 gpx = gpxpy.parse(gpx_file)
 points=gpx.tracks[0].segments[0].points
 
-
-# In[7]:
-
-
 date_debut= gpx.get_time_bounds().start_time.astimezone().strftime("le %d %m %Y à %H heures %M minutes %S secondes")
 date_fin= gpx.get_time_bounds().end_time.astimezone().strftime("le %d %m %Y à %H heures %M minutes %S secondes")
 print("\nHeure de début : ",date_debut+".","\nHeure de fin :",date_fin+".\n")
@@ -47,18 +40,12 @@ print("\nHeure de début : ",date_debut+".","\nHeure de fin :",date_fin+".\n")
 
 # Il faut centrer la carte. Pour cela on calcule la moyenne des latitudes et des longitudes:
 
-# In[8]:
-
-
 l=[(p.latitude,p.longitude) for p in points]
 c=[sum(x) for x in zip(*l)]
 center=(c[0]/len(l),c[1]/len(l))# le "centre" (la moyenne).
 
 
 # Et on crée la carte, centrée en "center". Le facteur de zoom initial est un peu pifométrique: 
-
-# In[10]:
-
 
 m = Map(basemap=usedmap,center=center, zoom=15,scroll_wheel_zoom=True)
 
@@ -74,10 +61,6 @@ m = Map(basemap=usedmap,center=center, zoom=15,scroll_wheel_zoom=True)
 
 
 type_line= "Ant" # type de ligne pour la trajectoire (toute autre avaleur que "Ant" produit une "PolyLine") 
-
-
-# In[12]:
-
 
 if type_line == "Ant":
     line= AntPath(
@@ -104,14 +87,7 @@ m.add_layer(line)
 # - La distance globale porcourue.
 # - Un "marker" tous les "delta" mêtres. 
 
-# In[14]:
-
-
 delta=1000. # 1 km.
-
-
-# In[26]:
-
 
 distance_parcourue=0.0
 dists=[]
@@ -130,9 +106,6 @@ for point in points:
 
 # Des marqueurs pour le début et la fin du parcours :
 
-# In[21]:
-
-
 circle_marker1 = CircleMarker(
     location = (points[-1].latitude,points[-1].longitude),
     radius = 7,color = "red",fill_color = "white")
@@ -144,32 +117,19 @@ marks+=[circle_marker1,circle_marker2]
 
 # Placer les marqueurs sur la carte:
 
-# In[22]:
-
-
 marker_cluster = MarkerCluster(
     markers=marks
 )
 m.add_layer(marker_cluster);
-
-
-# In[23]:
-
 
 print("Distance totale parcourue :",distance_parcourue,"mêtres.")
 
 
 # ### La carte : ###
 # 
-# _(zoomez éventuellement pour voir tous les marqueurs)._
-
-# In[ ]:
-
 
 
 # ### L'altitude au cours du parcours : ###
-
-# In[38]:
 
 
 p = figure(title="Altitude /distance", x_axis_label='Distance parcourue', y_axis_label='altitude',
@@ -182,15 +142,10 @@ p.line(dists,[point.elevation for point in points] , line_width=2)
 # 
 # Attention le gps n'est pas très précis pour les altitudes !
 
-# In[28]:
-
 
 z=[x[1]-x[0] for x in zip([point.elevation for point in points][1:],[point.elevation for point in points][:-1])]
 up= reduce(lambda a,b: a+max(b,0),z)
 down= reduce(lambda a,b: a+max(-b,0),z)
-
-
-# In[29]:
 
 
 print("montée :  %8.2f"% (up),", descente : %8.2f"% (down),"(mêtres).")
@@ -198,16 +153,11 @@ print("montée :  %8.2f"% (up),", descente : %8.2f"% (down),"(mêtres).")
 
 # ### Vitesse (en km/h) en fonction du temps (en secondes) : ###
 
-# In[34]:
-
 
 start= points[0].time
 z=[((x[0]-x[1]).total_seconds(),x[1]) for x in zip([point.time for point in points][1:],                                                    [point.time for point in points][:-1])]
 d=[x[1].distance_3d(x[0]) for x in zip(points[1:],points[:-1])]
 vt=[(x[0]/x[1][0],(x[1][1]-start).total_seconds())  for x in zip(d,z) if x[1][0]>0]
-
-
-# In[39]:
 
 
 pv = figure(title="Vitesse / temps", x_axis_label='temps', y_axis_label='Vitesse (km/h)',width=800,height=300)
@@ -217,15 +167,12 @@ pv.line([v[1] for v in vt],[v[0]*3.6 for v in vt],  line_width=2)
 
 # ### Vitesse moyenne : ###
 
-# In[36]:
-
 
 print("Vitesse moyenne : %8.2f"% (3.6*distance_parcourue/(points[-1].time - points[0].time).total_seconds()),"km/h.")
 
 
 # Vitesse en fonction de la déclivité
 
-# In[37]:
 
 
 dec0= [x[1]-x[0] for x in zip([point.elevation for point in points[1:]],[point.elevation for point in points[:-1]])]
@@ -233,10 +180,13 @@ dec = [x[0] for x in zip(dec0,z) if x[1][0]>0]
 pdec= figure(title="Vitesse / déclivité ", x_axis_label='declivité', y_axis_label='Vitesse (km/h)',width=800,height=300)
 pdec.circle(dec,[3.6*x[0] for x in vt],size=6, color="red", alpha=0.5)
 
-show(column(p,pv,pdec))
 
 display(m)
-# In[ ]:
+
+show(column(p,pv,pdec))
+
+
+
 
 
 
